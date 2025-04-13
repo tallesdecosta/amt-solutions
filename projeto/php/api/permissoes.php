1e70982b053721f2c4ce5ff402e0b9761113e235
Talles da Costa <107369815+tallesdecosta@users.noreply.github.com>
2025-04-13 04:08:41 -0300

    feat: logout & gerenciar permissões + start page

--- projeto/php/api/permissoes.php
+++ projeto/php/api/permissoes.php
@@ -8,6 +8,10 @@
             echo json_encode(retornarPermissoes());
             break;
         
+        case "PUT":
+            echo json_encode(atualizarPermissoes());
+            break;
+
         default:
             # code...
             break;
@@ -15,13 +19,34 @@
 
     function retornarPermissoes() {
 
-        $conn = conectar();
         $sql = "SELECT * FROM permissao WHERE id_usuario = '".$_SESSION['id']."'";
+
+        if(isset($_GET['id'])) {
+
+            $sql = "SELECT * FROM permissao WHERE id_usuario = '".$_GET['id']."'";
+
+        }
+
+        $conn = conectar();
+        
         $res = $conn -> query($sql);
         $data = $res -> fetch_assoc();
         return $data;
 
     } 
 
+    function atualizarPermissoes() {
 
+        if(isset($_GET['id'])) {
+
+            $sql = "UPDATE permissao SET estoque = '".$_GET['estoque']."', vendas = '".$_GET['vendas']."', financeiro='".$_GET['financeiro']."', gestao='".$_GET['gestao']."'  WHERE id_usuario = '".$_GET['id']."'";
+
+            $conn = conectar();
+        
+            $res = $conn -> query($sql);
+
+            return http_response_code(200);
+
+        }
+    }
 ?>
