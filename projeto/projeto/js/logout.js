576cf4793db5535eac11f827ee96e41de89ef211
Talles da Costa <107369815+tallesdecosta@users.noreply.github.com>
2025-04-14 22:21:59 -0300

    fix: fiz besteira

--- projeto/projeto/js/logout.js
+++
@@ -1,8 +0,0 @@
-async function logout() { 
-
-    await fetch('../php/logout.php', {
-        method: "GET",
-        credentials: "include"
-    }); 
-    location.reload();
-}
--- projeto/projeto/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-
-    session_destroy();
-    http_response_code(200);
-
-?>
