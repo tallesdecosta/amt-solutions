08ce91be4d5bba37a46905f32638b4886b3a0293
Talles da Costa <107369815+tallesdecosta@users.noreply.github.com>
2025-04-26 19:40:19 -0300

    Add files via upload

---
+++ projeto/js/checarAcesso.js
@@ -0,0 +1,21 @@
+async function checarAcesso(modulo) {
+
+    res = await fetch('../php//api/permissoes.php', {
+
+        method: "GET",
+        credentials: "include"
+
+    });
+
+
+    arr = await res.json()
+
+    for (i in arr) {
+
+        if (i == modulo && arr[i] == 0) {
+
+            window.location = "startPage.html";
+        }
+
+    }
+}
