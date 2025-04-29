8d83921a0c4ae01662d5cc864f5e29c85989c22a
Matheus-ls1 <166852383+Matheus-ls1@users.noreply.github.com>
2025-04-29 14:12:50 -0300

    Add files via upload

--- projeto/js/entradaDeCaixa.js
+++ projeto/js/entradaDeCaixa.js
@@ -1,20 +1,62 @@
+async function buscarfunc() {
+     let data = await fetch("../php/entradaDeCaixa.php",{
+
+        method: "GET",
+        credentials: "include",
+     })
+
+     if(data){
+        return await data.json()
+     }
+}
+
+async function adcfunc() {
+
+    let select = document.getElementById('respon')
+
+    let response = await buscarfunc()
+
+    if(response){
+
+        for(i in response){
+
+            let opt = ' <option value="'+ response[i].id_usuario +'">'+ response[i].nome +'</option> '
+
+            select.innerHTML += opt
+        }
+    }
+
+    select.value = 0;
+
+    console.log(select.value)
+    
+}
+
+
+
 async function salvar() {
     let element1 = document.getElementById("respon").value
     let element2 = document.getElementById("valor").value
-    let element3 = document.getElementById("obs").value
+    let element3 = document.getElementById("obs").value 
 
-    response = await registrarop(element1, element2, element3)
+    if(element1 != 0){
+        response = await registrarop(element1, element2, element3)
 
-    for (i in response) {
-
-        if (response.result = 200) {
-            alert("A operação de entrada de caixa foi realizada com sucesso!")
-
-            window.location.href = '../html/moduloVendas.html'
-        } else if (response.result = 0) {
-            console.log("Encontramos problemas no servidor, Pedimos que tente novamente mais tarde")
+        for (i in response) {
+    
+            if (response.result = 200) {
+                alert("A operação de entrada de caixa foi realizada com sucesso!")
+    
+                window.location.href = '../html/moduloVendas.html'
+            } else if (response.result = 0) {
+                console.log("Encontramos problemas no servidor, Pedimos que tente novamente mais tarde")
+            }
         }
+    }else if(element1 == 0){
+        alert('Para salvar a entrada de caixa é preciso preencher o campo "Responsável"')
     }
+
+    
 }
 
 async function registrarop(dado1, dado2, dado3) {
@@ -48,6 +90,9 @@ async function verifacess() {
                 let element = document.getElementById('verifacess')
 
                 element.style.display = 'none'
+
+                adcfunc()
+
             }else{
                 alert("Para acessar o módulo de Entrada de Caixa a conta deve conter privilegio de gestor")
             }
