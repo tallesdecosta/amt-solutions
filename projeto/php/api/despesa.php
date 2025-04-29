b4cb6fea0cbbd7fb997017b681449b10a3f96ede
Talles da Costa <107369815+tallesdecosta@users.noreply.github.com>
2025-04-29 18:11:09 -0300

    Add files via upload

---
+++ projeto/php/api/despesa.php
@@ -0,0 +1,83 @@
+<?php
+
+    session_start();
+    require '../conectar_bd.php';
+
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            echo json_encode(retornarDespesa());
+            break;
+        
+        case "POST":
+            break;
+
+        case "DELETE":
+            break;
+
+        default:
+            # code...
+            break;
+    }
+
+    function retornarDespesa() {
+
+        $sql = "SELECT descritivo, valor FROM despesa WHERE dataInicio BETWEEN '" . $_GET['inicio'] . "' AND '" . $_GET['fim'] . "'";
+
+            $conn = conectar();
+                    
+
+            $res = $conn->query($sql);
+
+            $id_produto = [];
+
+            while ($linha = $res->fetch_assoc()) {
+
+                $id_produto[] = $linha;
+                
+            }
+
+            return $id_produto;
+        }
+
+    
+
+function salvarDfc() {
+
+    $sql = "INSERT INTO dfc(titulo, id_usuario, dataInicio, dataFinal) VALUES('".$_POST['titulo']."','".$_SESSION['id']."','".$_POST['data-inicio']."', '".$_POST['data-fim']."')";
+
+    $conn = conectar();
+                    
+
+    $res = $conn->query($sql);
+
+    if($res) {
+
+        return ['status' => 'ok'];
+    } else {
+
+        return ['status' => 'erro'];
+
+    }
+
+}
+
+function deletarDfc() {
+
+    $sql = "DELETE FROM dfc WHERE id_dfc = '".$_GET['id']."'";
+
+    $conn = conectar();
+
+    $res = $conn->query($sql);
+
+    if($res) {
+
+        return ['status' => 'ok'];
+    } else {
+
+        return ['status' => 'erro'];
+
+    }
+
+}
+
+?>
