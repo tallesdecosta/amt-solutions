194f4f17d850c2c194f1ff3b911ef90346dc32d7
Matheus-ls1 <166852383+Matheus-ls1@users.noreply.github.com>
2025-05-31 21:43:27 -0300

    Add files via upload

--- projeto/js/login.js
+++ projeto/js/login.js
@@ -2,59 +2,55 @@ btnEntrar = document.getElementById('btn-entrar');
 
 btnEntrar.addEventListener('click', async () => {
 
-    try {
-    if (!document.getElementById('username').checkValidity()) {
+    user = document.getElementById('username').value;
+    senha = document.getElementById('senha').value;
+    body = new URLSearchParams();
+    body.append("user", user);
+    body.append("senha", senha);
 
-        document.getElementById('username').reportValidity(); 
-        return;
+    data = await fetch("../php/login.php", {
 
+        method: "POST",
+        body: body
+
+    });
+
+    if (data.ok) {
+
+        window.location = "startPage.html";
+
+    } else {
+
+        alert("Senha ou usuário incorretos. Por favor, tente novamente e, caso necessite de troca, converse com seu administrador.")
     }
+});
 
-    if (!document.getElementById('senha').checkValidity()) {
+form = document.getElementById('form1');
 
-        document.getElementById('senha').reportValidity(); 
-        return;
+form.addEventListener('keydown', async (event) => {
 
+    if (event.keyCode === 13) {
+
+        user = document.getElementById('username').value;
+        senha = document.getElementById('senha').value;
+        body = new URLSearchParams();
+        body.append("user", user);
+        body.append("senha", senha);
+
+        data = await fetch("../php/login.php", {
+
+            method: "POST",
+            body: body
+
+        });
+
+        if (data.ok) {
+
+            window.location = "startPage.html";
+
+        } else {
+
+            alert("Senha ou usuário incorretos. Por favor, tente novamente e, caso necessite de troca, converse com seu administrador.")
+        }
     }
-
-            user = document.getElementById('username').value;
-            senha = document.getElementById('senha').value;
-            body = new URLSearchParams();
-            body.append("user", user);
-            body.append("senha", senha);
-
-            data = await fetch("../php/login.php", {
-
-                method: "POST",
-                body: body
-
-            });
-
-
-            if (!data.ok) {
-                throw new Error(`HTTP Error! Status: ${data.status}`)
-
-            }
-
-            data = await data.json();
-
-            if (data.correto) {
-
-                window.location = "startPage.html";
-                
-            } else if (data.correto == false) {
-
-                alert("Senha ou usuário incorretos. Por favor, tente novamente e, caso necessite de troca, converse com seu administrador.")
-                
-
-            }
-        
-    } catch (error) {
-
-        alert("Ocorreu um problema na comunicação com nosso servidor interno. Por favor, aguarde alguns minutos e entre em contato com o suporte caso o problema persista.")
-        console.log(error)
-        
-    }
-
-    
 });
